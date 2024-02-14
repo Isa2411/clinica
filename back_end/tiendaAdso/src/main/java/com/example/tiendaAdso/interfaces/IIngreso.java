b25b8856da7fd61e395ff3d2909474468e264b11
Isabela Cordoba <isabelacordobaguti@gmail.com>
2024-02-14 17:55:06 -0500

    Se eliminó la carpeta del back_end y front_end

--- back_end/tiendaAdso/src/main/java/com/example/tiendaAdso/interfaces/IIngreso.java
+++
@@ -1,8 +0,0 @@
-package com.example.tiendaAdso.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.tiendaAdso.models.ingreso;
-
-public interface IIngreso extends CrudRepository<ingreso,String> {
-
-}
--- back_end/tiendaAdso/src/main/java/com/example/tiendaAdso/interfacesService/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.example.tiendaAdso.interfacesService;
