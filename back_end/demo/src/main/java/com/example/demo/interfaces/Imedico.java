43db13c9534d45f67df0867af61ea09048394e6d
Carolina Martinez <carolina22cmc@gmail.com>
2024-02-24 09:44:39 -0500

    Eliminar spring

--- back_end/demo/src/main/java/com/example/demo/interfaces/Imedico.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.model.medico;
-
-public interface Imedico extends CrudRepository<medico,String> {
-
-}
--- back_end/demo/src/main/java/com/example/demo/interfaces/Ipaciente.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.model.paciente;
-
-public interface Ipaciente extends CrudRepository<paciente,String>{
-
-}
