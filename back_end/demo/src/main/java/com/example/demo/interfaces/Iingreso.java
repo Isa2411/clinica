23aeac8770c528b09b7d32f32b9e0532eea7fb47
Isa.24112005 <isabelacordobaguti@gmail.com>
2024-03-03 14:05:44 -0500

    se elimino el bakck end y el front end

--- back_end/demo/src/main/java/com/example/demo/interfaces/Iingreso.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.demo.model.ingreso;
-
-public interface Iingreso extends CrudRepository<ingreso,String>{
-
-}
