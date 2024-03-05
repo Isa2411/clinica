16d6f267bce0f980aff6a20e5555161964d5f487
unknown <mariajosemurciamartinez85@gmail.com>
2024-03-05 12:55:42 -0500

    Se actualizaron los js de medico, ingreso y paciente

--- front_end/js/ingreso.js
+++ front_end/js/ingreso.js
@@ -1,5 +1,5 @@
 //se almacena la url de la api
-let url = "http://localhost:8080/api/v1/ingreso/";
+let url = "http://localhost:8081/api/v1/ingreso/";
 function listarIngreso() {
   $.ajax({
     url: url,
--- front_end/js/medico.js
+++ front_end/js/medico.js
@@ -1,5 +1,5 @@
 //se almacena la url de la api
-let url = "http://localhost:8080/api/v1/medico/";
+let url = "http://localhost:8081/api/v1/medico/";
 function listarMedico() {
     $.ajax({
         url: url,
