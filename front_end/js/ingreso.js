0f0a0bdb6490aba6f6ac28e8ce580ae80dfbe7ba
Isabela Cordoba <isabelacordobaguti@gmail.com>
2024-03-21 23:13:45 -0500

    Se actualizó el html de lista ingreso y el js de ingreso

--- front_end/js/ingreso.js
+++ front_end/js/ingreso.js
@@ -260,19 +260,19 @@ function registrarIngreso() {
   let fechasalida = document.getElementById("fechasalida").value;
   let estado = document.getElementById("estado").value;
 
- // Convertir las fechas a objetos Date para compararlas
- let fechaIngresoDate = new Date(fechaingreso);
- let fechaSalidaDate = new Date(fechasalida);
- 
- // Verificar si la fecha de salida es anterior a la fecha de ingreso
- if (fechaSalidaDate < fechaIngresoDate) {
-   Swal.fire({
-     title: "Error.",
-     text: "La fecha de salida no puede ser anterior a la fecha de ingreso.",
-     icon: "error"
-   });
-   return; // Detener la ejecución de la función
- }
+  // Convertir las fechas a objetos Date para compararlas
+  let fechaIngresoDate = new Date(fechaingreso);
+  let fechaSalidaDate = new Date(fechasalida);
+
+  // Verificar si la fecha de salida es anterior a la fecha de ingreso
+  if (fechaSalidaDate < fechaIngresoDate) {
+    Swal.fire({
+      title: "Error.",
+      text: "La fecha de salida no puede ser anterior a la fecha de ingreso.",
+      icon: "error"
+    });
+    return; // Detener la ejecución de la función
+  }
 
 
   let formData = {
@@ -494,39 +494,38 @@ function actualizarIngreso() {
 }
 
 function filtrarIngreso() {
-  var filtro = document.getElementById('filtroIngreso').value;
-  var urlFiltro = '';
-  if (filtro == '') {
-    urlFiltro = url;
-  } else {
-    urlFiltro = url + "busquedafiltro/" + filtro
-  }
-  $(document).ready(function () {
-    $.ajax({
-      url: urlFiltro,
-      method: 'GET',
-      dataType: 'json',
-      success: function (data) {
-        var tabla = $('#cuerpoTablaingreso tbody');
-        tabla.empty();
-
-        $.each(data, function (index, item) {
-          tabla.append('<tr id="dato' + item.id + '">' +
-            '<td>' + item.id_ingreso + '</td>' +
-            '<td>' + item.habitacion + '</td>' +
-            '<td>' + item.cama + '</td>' +
-            '<td>' + item.fecha_ingreso + '</td>' +
-            '<td>' + item.fecha_salida + '</td>' +
-            '<td>' + item.medico.primer_nombre + '</td>' +
-            '<td>' + item.paciente.primer_nombre + '</td>' +
-            '<td>' + item.estado + '</td>' +
-            '<td><button class="btn btn-success actualizar" data-id="' + item.id_ingreso + '">Editar</button><button id="' + item.id_ingreso + '" class="eliminar-dato btn btn-danger" data-id="' + item.id_ingreso + '">Eliminar</button></td>' +
-            '</tr>');
-        });
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log('Error: ' + textStatus, errorThrown);
+  var filtro = document.getElementById("filtroIngreso").value.toLowerCase();
+  var fechaIngreso = document.getElementById("fechaIngreso").value;
+  var fechaSalida = document.getElementById("fechaSalida").value;
+  var tabla = document.getElementById("cuerpoTablaingreso");
+  var tr = tabla.getElementsByTagName("tr");
+  for (var i = 0; i < tr.length; i++) {
+    var tdCodigo = tr[i].getElementsByTagName("td")[0];
+    var tdHabitacion = tr[i].getElementsByTagName("td")[1];
+    var tdCama = tr[i].getElementsByTagName("td")[2];
+    var tdFechaIngreso = tr[i].getElementsByTagName("td")[3];
+    var tdFechaSalida = tr[i].getElementsByTagName("td")[4];
+    var tdMedico = tr[i].getElementsByTagName("td")[5];
+    var tdPaciente = tr[i].getElementsByTagName("td")[6];
+    var tdEstado = tr[i].getElementsByTagName("td")[7];
+    if (tdCodigo || tdHabitacion || tdCama || tdFechaIngreso || tdFechaSalida || tdMedico || tdPaciente || tdEstado) {
+      var codigoValue = tdCodigo.textContent || tdCodigo.innerText;
+      var habitacionValue = tdHabitacion.textContent || tdHabitacion.innerText;
+      var camaValue = tdCama.textContent || tdCama.innerText;
+      var fechaIngresoValue = tdFechaIngreso.textContent || tdFechaIngreso.innerText;
+      var fechaSalidaValue = tdFechaSalida.textContent || tdFechaSalida.innerText;
+      var medicoValue = tdMedico.textContent || tdMedico.innerText;
+      var pacienteValue = tdPaciente.textContent || tdPaciente.innerText;
+      var estadoValue = tdEstado.textContent || tdEstado.innerText;
+      if ((codigoValue.toLowerCase().indexOf(filtro) > -1 || habitacionValue.toLowerCase().indexOf(filtro) > -1 ||
+        camaValue.toLowerCase().indexOf(filtro) > -1 || medicoValue.toLowerCase().indexOf(filtro) > -1 ||
+        pacienteValue.toLowerCase().indexOf(filtro) > -1 || estadoValue.toLowerCase().indexOf(filtro) >
+        -1) &&
+        (fechaIngresoValue.includes(fechaIngreso) || fechaSalidaValue.includes(fechaSalida))) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
-    });
-  });
+    }
+  }
 }
